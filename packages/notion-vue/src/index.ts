6ccfbfeeebf29a3313efdf2e9d96eb21d5d2b12b
Minsu Lee <amond@amond.net>
2025-12-18 00:59:04 +0900

    feat(notion-vue): add gallery styles and exports T011-T013
    - Add gallery CSS styles for grid layout and cards
    - Export new collection components and composables
    - Update TODO.md to mark Gallery View completed

--- packages/notion-vue/src/index.ts
+++ packages/notion-vue/src/index.ts
@@ -7,10 +7,23 @@ export { default as NotionRenderer } from './components/NotionRenderer.vue'
 export { default as NotionText } from './components/NotionText.vue'
 export { default as NotionTOC } from './components/NotionTOC.vue'
 
+// Collection Components
+export { default as CollectionCard } from './components/collection/CollectionCard.vue'
+export { default as CollectionCardCoverComponent } from './components/collection/CollectionCardCover.vue'
+export { default as CollectionGroup } from './components/collection/CollectionGroup.vue'
+export { default as CollectionProperty } from './components/collection/CollectionProperty.vue'
+export { default as CollectionViewGallery } from './components/collection/CollectionViewGallery.vue'
+
 // Composables
 export { provideNotionContext, useNotionContext } from './composables/useNotionContext'
 export { useTableOfContents } from './composables/useTableOfContents'
 export type { TableOfContentsEntry } from './composables/useTableOfContents'
+export { useCollectionData } from './composables/useCollectionData'
+export type { UseCollectionDataOptions, UseCollectionDataReturn } from './composables/useCollectionData'
+export { useCollectionGroups } from './composables/useCollectionGroups'
+export type { UseCollectionGroupsOptions, UseCollectionGroupsReturn } from './composables/useCollectionGroups'
+export { useCollectionCover } from './composables/useCollectionCover'
+export type { UseCollectionCoverOptions, UseCollectionCoverReturn } from './composables/useCollectionCover'
 
 // Types
 export type {
@@ -20,6 +33,18 @@ export type {
   NotionContext,
   NotionRendererProps,
 } from './types'
+
+// Collection Types
+export type {
+  CollectionCardCover,
+  CollectionCardCoverAspect,
+  CollectionCardCoverSize,
+  CollectionCardProps,
+  CollectionGroupData,
+  CollectionPropertyConfig,
+  CollectionViewProps,
+  GalleryCollectionViewFormat,
+} from './types/collection'
 
 // Utils
 export {
